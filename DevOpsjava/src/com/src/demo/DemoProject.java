1ea23e92458afca4f52cc3e090fa10a8f8336eb4
[srinath gyajangi] <[srinathg363@gmail.com]>
2022-11-04 15:30:24 -0500

    new project

---
+++ DevOpsjava/src/com/src/demo/DemoProject.java
@@ -0,0 +1,15 @@
+package com.src.demo;
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		for(int i =1; i<=5; i++) {
+			
+			for(int j =1;j<=i;j++) {
+				System.out.print(" "+j);
+			}System.out.println();
+		}
+
+	}
+
+}
